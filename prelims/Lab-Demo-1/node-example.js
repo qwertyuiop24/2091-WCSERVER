a77574168d37a75dc780fb7654ed9703a9e94945
qwertyuiop24 <87112740+qwertyuiop24@users.noreply.github.com>
2023-01-23 12:16:17 +0800

    Add files via upload
    New prelims changes

---
+++ prelims/Lab-Demo-1/node-example.js
@@ -0,0 +1,14 @@
+//Name: Layson, Aron E.
+//Module 3
+//Week 2 and 3: Basic of NodeJs
+
+
+//First Program
+console.log("Welcome to Earth");
+
+//Creating my first function
+function Display(x){
+    console.log(x);
+}
+
+Display(200);
