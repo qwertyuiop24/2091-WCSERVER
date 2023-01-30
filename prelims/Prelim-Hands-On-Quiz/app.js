b596586458bad540acddeea06d39b63f9ff72128
qwertyuiop24 <87112740+qwertyuiop24@users.noreply.github.com>
2023-01-30 23:17:54 +0800

    2 files modified

--- prelims/Prelim-Hands-On-Quiz/app.js
+++ prelims/Prelim-Hands-On-Quiz/app.js
@@ -1,5 +1,12 @@
+//Layson, Aron E.
+//WD - 201
+//Prelims Hands-On Quiz
+//Prof. Joseph A. Esquivel
+
+//to include external modules calculation.js
 var calcu = require('./calculation.js');
 
+//declaring variables
 var rate = 300;
 var hours = 4;
 var days = 6;
@@ -9,19 +16,19 @@ var sss = 1200;
 var pagibig = 300;
 var philhealth = 400;
 
+//To calculate/solve the problem
 var gpay = calcu.multiply(rate, hours);
 var gpay2 = calcu.multiply(gpay, days);
 var tax1 = calcu.multiply(gpay2, tax);
 var deduct = calcu.add(tax1, sss);
 var deduct2 = calcu.add(pagibig, philhealth);
 var deduct3 = calcu.add(deduct, deduct2);
-var netsale = calcu.subtract(gpay2, deduct3);
 
-console.log("The Gross Income: " + gpay2);
-console.log("Tax: " + tax1);
-console.log("SSS: " + sss);
-console.log("Pag-Ibig Fund: " + pagibig);
-console.log("PhilHealth: " + philhealth);
-console.log("Total Deductions: " +deduct3);
-console.log("The Net Salary: " + netsale);
-
+//Print the output
+console.log('The Gross Income: ' + gpay2);
+console.log('Tax: ' + tax1);
+console.log('SSS: ' + sss);
+console.log('Pag-Ibig Fund: ' + pagibig);
+console.log('PhilHealth: ' + philhealth);
+console.log('Total Deductions: ' + deduct3);
+console.log('The Net Salary: ' + calcu.subtract(gpay2, deduct3));
--- prelims/Prelim-Hands-On-Quiz/calculation.js
+++ prelims/Prelim-Hands-On-Quiz/calculation.js
@@ -1,15 +1,22 @@
+//Layson, Aron E.
+//WD - 201
+
+//add
 function add(a, b) {
   return a + b;
 }
 
+//subtract
 function subtract(a, b) {
   return a - b;
 }
 
+//multiply
 function multiply(a, b) {
   return a * b;
 }
 
+//divide
 function divide(a, b) {
   return a / b;
 }
