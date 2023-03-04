f5ddf7a43a0025298258dc0b3e7d73f310a3ed9d
qwertyuiop24 <87112740+qwertyuiop24@users.noreply.github.com>
2023-03-04 20:45:32 +0800

    1 file modified

--- midterms/mid-serve-static/index.js
+++ midterms/mid-serve-static/index.js
@@ -5,6 +5,12 @@
 const express = require('express');
 const app = express();
 
+//require body-parser for this method
+var bodyParser = require('body-parser');
+
+//Create application/x-www-form-urlencoded paser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 //4. Use the middleware required for serving static files
 
 app.use(express.static('public'));
@@ -15,6 +21,15 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/' + 'index.html');
     throw new Error('Error 404');
 });
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+    response = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+  });
 
 //Setting the listener to ENV PORT info
 
