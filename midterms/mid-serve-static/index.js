df2762894b9f8dc663ca84e65481f4ee85e5b401
qwertyuiop24 <87112740+qwertyuiop24@users.noreply.github.com>
2023-03-04 23:19:20 +0800

    2 files modified

--- midterms/mid-serve-static/index.js
+++ midterms/mid-serve-static/index.js
@@ -48,18 +48,40 @@ app.post('/uploads', upload.single('myFile'), (req, res) => {
   res.sendFile(path.join(__dirname, 'file-uploaded.html'));
 });
 
-app.get('/', function (req, res) {
+//create the route to serve a static index.html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/' + 'index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/' + 'about.html');
+});
+
+app.get('/blog', (req, res) => {
+  res.sendFile(__dirname + '/' + 'blog.html');
+});
+
+app.get('/contact', (req, res) => {
   res.sendFile(__dirname + '/' + 'contact.html');
 });
 
-app.get('/process_get', urlencodedParser, function (req, res) {
-  response = {
-    name: req.query.name,
-    sub_ject: req.query.sub_ject,
-    mes_sage: req.query.mes_sage,
-    em_ail: req.query.em_ail,
-  };
+app.get('/upload', (req, res) => {
+  res.sendFile(__dirname + '/' + 'upload.html');
+});
 
+//rerouting
+app.get('ab*cd', function (req, res) {
+  res.send('Error');
+});
+
+//forms in contacts
+app.post('/process_post', urlencodedParser, function (req, res) {
+  response = {
+    name: req.body.name,
+    subject: req.body.subject,
+    message: req.body.message,
+    email: req.body.email,
+  };
   console.log(response);
   res.end(JSON.stringify(response));
 });
