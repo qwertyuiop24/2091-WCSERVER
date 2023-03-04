0fcb50b3fa84d0492159c72bf3f7f30066ec4c54
qwertyuiop24 <87112740+qwertyuiop24@users.noreply.github.com>
2023-03-04 21:50:19 +0800

    1 file modified

--- midterms/mid-serve-static/index.js
+++ midterms/mid-serve-static/index.js
@@ -16,6 +16,22 @@ const fileStorage = multer.diskStorage({
   },
 });
 
+const checkFileType = function (file, cb) {
+      //Allowed file extensions
+      const fileTypes = /jpeg|jpg|png|gif|svg/;
+
+ //check extension names
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+
+  const mimeType = fileTypes.test(file.mimetype);
+
+  if (mimeType && extName) {
+    return cb(null, true);
+  } else {
+    cb("Error: You can Only Upload Images!!");
+  }
+};
+
 const upload = multer({ storage: fileStorage });
 
 app.post('/uploads', upload.single('myFile'), (req, res) => {
