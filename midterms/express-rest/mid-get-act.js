64b82e059339dbdfbaf0ed6d037a9660f7fed110
qwertyuiop24 <87112740+qwertyuiop24@users.noreply.github.com>
2023-03-06 23:16:42 +0800

    1 binaries uploaded

---
+++ midterms/express-rest/mid-get-act.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+//Array for GET Method
+const movies = [{
+    id: 1,
+    title:'Superman'
+},
+{
+    id: 2,
+    title: 'Thor'
+},
+{
+    id: 3,
+    title: 'Iron Man'
+}
+];
+
+// get all movies 
+app.get('/api/movies', (req, res) => { 
+    res.send(movies); 
+});
+
+//get movie by title
+app.get('/api/movies/:title', (req, res) => {
+    const movie = movies.find(h => h.title === parseInt(req.params.title));
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.'); res.send(movie); 
+});
+
+app.listen(4000, () => console.log('Listening on port 4000'));
