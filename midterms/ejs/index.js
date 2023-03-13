084c67a7a882981ea817307116e445596fcb03b8
qwertyuiop24 <87112740+qwertyuiop24@users.noreply.github.com>
2023-03-13 12:43:09 +0800

    4 files modified, 1 added, 1 deleted

--- midterms/ejs/index.js
+++ midterms/ejs/index.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = express();
 
-// set the view engine to Pug
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 // serve static files
 app.use(express.static('public'));
 
-// render the index page
 app.get('/', (req, res) => {
-  res.render('index', { title: 'My Pug App' });
+  res.render('index', { title: 'My EJS App' });
 });
 
-// start the server
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
