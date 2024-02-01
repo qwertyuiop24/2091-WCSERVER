e460f3982fc8f4f93ca730bcffe95429c76643fd
qwertyuiop24 <87112740+qwertyuiop24@users.noreply.github.com>
2024-02-01 16:06:59 +0800

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { JoinComponent } from './join/join.component';
+import { PartnersComponent } from './partners/partners.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'join',
+    component: JoinComponent
+  },
+  {
+    path: 'partners',
+    component: PartnersComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
